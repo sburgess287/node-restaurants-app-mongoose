ef9e08dfd9569b0d366a28467a5291e25272c729
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-06 11:57:10 -0800

    updated config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://new-user-one:Lemur73!@ds041678.mlab.com:41678/node-restaurants-app-mongoose';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
