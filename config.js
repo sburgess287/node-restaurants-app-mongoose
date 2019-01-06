10f96fe1ffc6ccd1acc1f91dfbc11da3c20eeba2
Sarah Burgess <sarahburgess287@gmail.com>
2019-01-06 11:56:06 -0800

    re-added localhost env variable because still showing API request failure on deploy to heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://new-user-one:Lemur73!@ds041678.mlab.com:41678/node-restaurants-app-mongoose';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
